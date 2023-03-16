9f0f84cd1b6502d021b90ec20ce1728c289731f3
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-16 22:29:09 +0300

    add .pop() function.

--- src/freecodecamp/JS/All.js
+++ src/freecodecamp/JS/All.js
@@ -1,3 +1,7 @@
 const myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3])
 //method push
+
+const myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.pop()
+//.pop() function.
