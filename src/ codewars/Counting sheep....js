a11ce65b7cd041db3ee3dae7c8319940bc903003
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-16 21:28:18 +0300

    add function ChuckSaysSo

---
+++ src/ codewars/Counting sheep....js
@@ -0,0 +1,16 @@
+var arrayOfSheep=[true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true];
+
+let count = 0;
+for (var i=0; i<arrayOfSheep.length;i++){
+    if(arrayOfSheep[i] === true) {count +=1;}
+
+}
+
+
+
+
