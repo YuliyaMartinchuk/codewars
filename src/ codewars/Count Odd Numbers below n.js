a48a2a4e0be475d44c654c2d13c7a505e3f5fcd0
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-21 23:00:05 +0300

    Add function = Count Odd Numbers below n new

--- src/ codewars/Count Odd Numbers below n.js
+++ src/ codewars/Count Odd Numbers below n.js
@@ -1,9 +1,3 @@
 function oddCount(n){
-    const newArr = [];
 
-    for (let i = 0; i < n; i++) {
-        if (i % 2 !== 0) {
-            newArr.push(i);
-        }
-    }
-    return newArr.length; }
+        return Math.floor(n/2) }
