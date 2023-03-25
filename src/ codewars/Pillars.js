6a0f5951d8f2ee8efd69e804c59174df1d9d0d1e
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-25 23:23:22 +0300

    add functiom DNA to RNA Conversion

--- src/ codewars/Pillars.js
+++ src/ codewars/Pillars.js
@@ -1,5 +1,6 @@
 function pillars(numPill, dist, width) {
-    if (numPill<2) {
+    if (numPill < 2) {
         return 0
-    }  return (numPill-1)*dist*100+width*(numPill-2)
+    }
+    return (numPill - 1) * dist * 100 + width * (numPill - 2)
 }
